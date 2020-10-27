859261edac150f74ab8bc24d4cedde5a30bf4725
Marc Steele <marcsteele2014@gmail.com>
2020-10-27 16:57:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? ==  true && @status == "pending"
+    if valid? ==  true && @status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
