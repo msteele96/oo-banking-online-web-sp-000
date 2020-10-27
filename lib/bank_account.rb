039a218e3619dd2724b6d8abd9b0aba1e783f4cd
Marc Steele <marcsteele2014@gmail.com>
2020-10-27 16:12:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -19,12 +19,12 @@ class BankAccount
   end
 
   def valid?
+    binding.pry
     if @status = "open" && @balance > 0
       return true
     else
       return false
     end
-    binding.pry
   end
 
   def close_account
